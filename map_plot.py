caae064a5c997138003e8d389e4647be1b92292f
jah128 <jah128@york.ac.uk>
2020-10-09 13:20:27 +0100

    Updated presets

--- map_plot.py
+++ map_plot.py
@@ -16,12 +16,21 @@ print("_________________________________________________________________________
 import os,csv, numpy as np,geopandas as gpd,pandas as pd,matplotlib.pyplot as plt, random, sys, time, pickle
 from datetime import datetime, timedelta
 
+batch_mode = True
+batch_list = ['ltla','msoa','lsoa','default','heatmap','nyorks-lsoa','northeast','northwest','southwest','southeast','midlands','east','yorkshire','northyorkshire']
+preset = 'default'
+
 debug = False
 overwrite_mode = True           #If set to false, program will halt if output folder exists
 data_path = "data"
 preset_path = "presets"
 output_path = "plots"
 
+if(os.path.isdir(output_path)):
+    if not overwrite_mode:
+        print("Output path %s already exists; aborting" % (output_path))
+        sys.exit()
+        
 map_filename = "zip://" + data_path + os.path.sep + "Middle_Layer_Super_Output_Areas__December_2011__Boundaries_EW_BSC-shp.zip"
 laa_map_filename = "zip://" + data_path + os.path.sep + "Local_Authority_Districts__May_2020__Boundaries_UK_BGC-shp.zip"
 lsoa_map_filename = "zip://" + data_path + os.path.sep + "Lower_Layer_Super_Output_Areas__December_2011__Boundaries_EW_BGC_v3-shp.zip"
@@ -31,21 +40,25 @@ lsoa_filename = data_path + os.path.sep + "lsoa.csv"
 cases_filename = data_path + os.path.sep + "casedata.csv"
 plt.rcParams['axes.facecolor']='#121240'
 
-preset = "ltla"
 
-#Load plot parameters from pickle file
-print("Loading preset " + preset)
-with open(preset_path + os.path.sep + preset + ".pickle","rb") as f: preset_data=pickle.load(f)        
-short_name,plot_msoa_boundaries,target_places,colour_map,msoa_colour_map,lsoa_colour_map,msoa_alpha,lsoa_alpha,frame_margins,label_x,label_y,title_x,title_y,plot_wales,plot_scotland,plot_towns,plot_laa,title_string,laa_linewidth,standalone_plot,post_process,resize_output,heat_lim,transparent,add_date,add_background,add_overlay,add_title,target_width,target_height,plot_laa_names,plot_laa_values,plot_ltla_data,plot_msoa_data,plot_lsoa_data,plot_combined_data,text_align_mode,date_font_size,title_font_size,laa_fontsize,mask_colour,add_footer,restrict_laa_to_targets,f_scale,overlay_filename,background_file = preset_data
+if(batch_mode):
+    print("RUNNING IN BATCH MODE")
+    preset_list = batch_list
+else:
+    preset_list = [preset]
+print("Plots to make: %s" % (preset_list))
 
-using_lsoa_data = plot_lsoa_data
+def load_parameters(preset_name):
+    global short_name,plot_msoa_boundaries,target_places,colour_map,msoa_colour_map,lsoa_colour_map,msoa_alpha,lsoa_alpha,frame_margins,label_x,label_y,title_x,title_y,plot_wales,plot_scotland,plot_towns,plot_laa,title_string,laa_linewidth,standalone_plot,post_process,resize_output,heat_lim,transparent,add_date,add_background,add_overlay,add_title,target_width,target_height,plot_laa_names,plot_laa_values,plot_ltla_data,plot_msoa_data,plot_lsoa_data,plot_combined_data,text_align_mode,date_font_size,title_font_size,laa_fontsize,mask_colour,add_footer,restrict_laa_to_targets,f_scale,overlay_filename,background_file
+    with open(preset_path + os.path.sep + preset_name + ".pickle","rb") as f: preset_data=pickle.load(f)        
+    short_name,plot_msoa_boundaries,target_places,colour_map,msoa_colour_map,lsoa_colour_map,msoa_alpha,lsoa_alpha,frame_margins,label_x,label_y,title_x,title_y,plot_wales,plot_scotland,plot_towns,plot_laa,title_string,laa_linewidth,standalone_plot,post_process,resize_output,heat_lim,transparent,add_date,add_background,add_overlay,add_title,target_width,target_height,plot_laa_names,plot_laa_values,plot_ltla_data,plot_msoa_data,plot_lsoa_data,plot_combined_data,text_align_mode,date_font_size,title_font_size,laa_fontsize,mask_colour,add_footer,restrict_laa_to_targets,f_scale,overlay_filename,background_file = preset_data
 
-output_subpath = output_path + os.path.sep + short_name
-if(os.path.isdir(output_subpath)):
-    if not overwrite_mode:
-        print("Output path %s already exists; aborting" % (output_subpath))
-        sys.exit()
-else: os.makedirs(output_subpath)
+using_lsoa_data = False
+
+for plot in preset_list:
+    load_parameters(plot)
+    if plot_lsoa_data: using_lsoa_data = True
+
 
 y_step = (frame_margins[3] - frame_margins[2]) / 2000.0
 
@@ -99,12 +112,7 @@ area_rates=[]
 file_date=datetime.strptime(time.ctime(os.path.getctime(cases_filename)),"%c")
 file_age=(datetime.today()-file_date).days
 if file_age > 0 : print ("Warning: The cases file is %d days old..." % (file_age))
-if not restrict_laa_to_targets: target_places=list(set([entry[0] for entry in cases_data]))
 
-for target in list.copy(target_places):
-    if target not in laa_names:
-        if debug: print("Target %s is not in LTLA list" % (target))
-        target_places.remove(target)
 
 print("________________________________________________________________________________")
 print("PROCESSING DATA")        
@@ -212,83 +220,102 @@ for day in range(number_of_days):
 
 print("________________________________________________________________________________")
 print("PRODUCING PLOTS")
-def_days = 30  #Plot since 10th March
+def_days = 30  #Plot since 1st March
 #def_days = 180 #Plot since start of August
-#def_days = 30
-#def_days=228
+def_days = 31
+#def_days=250
+print("Plotting %d days of data [%s to %s]" % (number_of_days-def_days, (start_date + timedelta(days=def_days)).strftime("%d/%m/%Y"), (start_date + timedelta(days=number_of_days - 1)).strftime("%d/%m/%Y") ))
+print("________________________________________________________________________________")
 
-for day in range(def_days,number_of_days):
-    c_date = start_date + timedelta(days=day)
-    f_string = output_subpath+os.path.sep+c_date.strftime("map-%Y%m%d.png")
-    print("Creating file %s" % (f_string))
-    fig,ax = plt.subplots(figsize=(36,36),frameon=not transparent)
-    ax.set_aspect('equal')
-    ax.axis(frame_margins)
-    
-    #divider = make_axes_locatable(ax)
-    #cax = divider.append_axes("bottom",size="5%",pad=0.1)
-    plt.axis('off')
-    z=0
-    if(plot_wales):
-        wales.plot(ax=ax,zorder=z,color=mask_colour)
-        z+=1
-    if(plot_scotland):
-        scotland.plot(ax=ax,zorder=z,color=mask_colour)
-        z+=1
-    if(plot_msoa_boundaries):
-        england.boundary.plot(ax=ax,zorder=z,linewidth=laa_linewidth,color='#888888')
-        z+=1
-    if(plot_ltla_data):
-        england.plot(column=c_date.strftime('ltla_%m%d'),ax=ax,cmap=colour_map,vmin=0,vmax=200,zorder=z)
-        z+=1
-    if(plot_combined_data):
-        england.plot(column=c_date.strftime('comb_%m%d'),ax=ax,cmap=colour_map,vmin=0,vmax=heat_lim,zorder=z)
-        z+=1
-    if(plot_msoa_data):
-        england.plot(column=c_date.strftime('msoa_%m%d'),ax=ax,cmap=msoa_colour_map,vmin=3,vmax=30,zorder=z,alpha=msoa_alpha)
-        z+=1
-    if(plot_lsoa_data):
-        lsoa_map.plot(column=c_date.strftime('lsoa_%m%d'),ax=ax,cmap=lsoa_colour_map,vmin=3,vmax=30,zorder=z,alpha=lsoa_alpha)
-        z+=1
-    if(plot_laa):
-        england_laa.boundary.plot(ax=ax,zorder=z,linewidth=laa_linewidth,color='#553311')
-        z+=1
-    if(plot_towns):
-        towns.plot(ax=ax,zorder=z,color='#111144')
-        z+=1
-    if(plot_laa_names or plot_laa_values):
-        #laa_centroids.plot(ax=ax,zorder=6,color='#33CC44')
-        for name in target_places:
-            count=laa_names.index(name)
-            val=laa_rates[count][day]
-            kx = laa_centroids[count][0]
-            ky = laa_centroids[count][1]
-            if kx > frame_margins[0] and kx < frame_margins[1] and ky > frame_margins[2] and ky < frame_margins[3]:                
-                #Plot labels text centered unless within 10% of RHS or LHS margin
-                al_mode = 'center'
-                if kx > ( (frame_margins[1] - frame_margins[0]) * 0.95) + frame_margins[0]: al_mode = 'right'
-                if kx < ( (frame_margins[1] - frame_margins[0]) * 0.05) + frame_margins[0]: al_mode = 'left'
-                y_shift = 0
-                if ky > ( (frame_margins[3] - frame_margins[2]) * 0.95) + frame_margins[2]: y_shift = -2 * (y_step * laa_fontsize)
-                if ky < ( (frame_margins[3] - frame_margins[2]) * 0.05) + frame_margins[2]: y_shift = 2 * (y_step * laa_fontsize)
-                yy_shift = 0
-                if plot_laa_names: 
-                    yy_shift = y_step * laa_fontsize
-                    plt.text(laa_centroids[count][0],laa_centroids[count][1]+y_shift+yy_shift,name,horizontalalignment=al_mode,fontsize=laa_fontsize*0.6,bbox=dict(boxstyle='square',color='#AAAA8877'))
-                if plot_laa_values: plt.text(laa_centroids[count][0],laa_centroids[count][1]+y_shift-yy_shift,"%3.1f" % val,horizontalalignment=al_mode,fontsize=laa_fontsize,bbox=dict(boxstyle='square',color='#FFFFEE11'))
-    if add_date: plt.text(label_x,label_y,c_date.strftime("%B %d"), horizontalalignment=text_align_mode, style='italic',fontsize=date_font_size)
-    if add_title:plt.text(title_x,title_y,title_string,horizontalalignment=text_align_mode,fontsize=title_font_size)
-    if add_footer:    
-        footer = file_date.strftime("Based on LTLA and MSOA case data from coronavirus.data.gov.uk, data set published %d/%m/%y. github.com/jah-photoshop/autocovid")
-        fr_scale = f_scale
-        if(plot_laa_values): 
-            footer = "Values are cases/100K/week. "+footer
-            fr_scale = f_scale * 1.2
-        plt.text(frame_margins[1]-( (frame_margins[1] - frame_margins[0]) * 0.01),frame_margins[2]+( (frame_margins[3] - frame_margins[2]) * 0.01),footer,horizontalalignment='right',fontsize=title_font_size / fr_scale, bbox=dict(boxstyle='square',color='#AAAA8844'))
-    plt.savefig(f_string, bbox_inches='tight')
-    if post_process:
-        if resize_output: os.system('convert %s -resize %dx%d\! %s' % (f_string,target_width,target_height,f_string))
-        if add_background: os.system('composite %s %s %s' % (f_string,background_file,f_string))
-        if add_overlay: os.system('composite %s %s %s' % (overlay_filename, f_string,f_string))
-    fig.clf()    
-
+#fig,ax = plt.subplots(figsize=(36,36),frameon=not transparent)
+#fig=plt.figure(figsize=(36,36),frameon=not transparent)
+for pre in preset_list:
+        
+    #Load plot parameters from pickle file
+    print("Plotting maps for preset " + pre)
+    load_parameters(pre)
+    fig=plt.figure(figsize=(36,36),frameon=not transparent)
+    output_subpath = output_path + os.path.sep + short_name
+    #print("Output subpath %s" % output_subpath)
+    if not os.path.exists(output_subpath): os.makedirs(output_subpath)
+    if not restrict_laa_to_targets: target_places=list(set([entry[0] for entry in cases_data]))
+    for target in list.copy(target_places):
+        if target not in laa_names:
+            if debug: print("Target %s is not in LTLA list" % (target))
+            target_places.remove(target) 
+    for day in range(def_days,number_of_days):
+        c_date = start_date + timedelta(days=day)
+        f_string = output_subpath+os.path.sep+c_date.strftime("map-%Y%m%d.png")
+        print("Creating file %s" % (f_string))
+        ax=plt.gca()
+        ax.set_aspect('equal')
+        ax.axis(frame_margins)
+        
+        #divider = make_axes_locatable(ax)
+        #cax = divider.append_axes("bottom",size="5%",pad=0.1)
+        plt.axis('off')
+        z=0
+        if(plot_wales):
+            wales.plot(ax=ax,zorder=z,color=mask_colour)
+            z+=1
+        if(plot_scotland):
+            scotland.plot(ax=ax,zorder=z,color=mask_colour)
+            z+=1
+        if(plot_msoa_boundaries):
+            england.boundary.plot(ax=ax,zorder=z,linewidth=laa_linewidth,color='#888888')
+            z+=1
+        if(plot_ltla_data):
+            england.plot(column=c_date.strftime('ltla_%m%d'),ax=ax,cmap=colour_map,vmin=0,vmax=200,zorder=z)
+            z+=1
+        if(plot_combined_data):
+            england.plot(column=c_date.strftime('comb_%m%d'),ax=ax,cmap=colour_map,vmin=0,vmax=heat_lim,zorder=z)
+            z+=1
+        if(plot_msoa_data):
+            england.plot(column=c_date.strftime('msoa_%m%d'),ax=ax,cmap=msoa_colour_map,vmin=3,vmax=30,zorder=z,alpha=msoa_alpha)
+            z+=1
+        if(plot_lsoa_data):
+            lsoa_map.plot(column=c_date.strftime('lsoa_%m%d'),ax=ax,cmap=lsoa_colour_map,vmin=3,vmax=30,zorder=z,alpha=lsoa_alpha)
+            z+=1
+        if(plot_laa):
+            england_laa.boundary.plot(ax=ax,zorder=z,linewidth=laa_linewidth,color='#553311')
+            z+=1
+        if(plot_towns):
+            towns.plot(ax=ax,zorder=z,color='#111144')
+            z+=1
+        if(plot_laa_names or plot_laa_values):
+            #laa_centroids.plot(ax=ax,zorder=6,color='#33CC44')
+            for name in target_places:
+                count=laa_names.index(name)
+                val=laa_rates[count][day]
+                kx = laa_centroids[count][0]
+                ky = laa_centroids[count][1]
+                if kx > frame_margins[0] and kx < frame_margins[1] and ky > frame_margins[2] and ky < frame_margins[3]:                
+                    #Plot labels text centered unless within 10% of RHS or LHS margin
+                    al_mode = 'center'
+                    if kx > ( (frame_margins[1] - frame_margins[0]) * 0.95) + frame_margins[0]: al_mode = 'right'
+                    if kx < ( (frame_margins[1] - frame_margins[0]) * 0.05) + frame_margins[0]: al_mode = 'left'
+                    y_shift = 0
+                    if ky > ( (frame_margins[3] - frame_margins[2]) * 0.95) + frame_margins[2]: y_shift = -2 * (y_step * laa_fontsize)
+                    if ky < ( (frame_margins[3] - frame_margins[2]) * 0.05) + frame_margins[2]: y_shift = 2 * (y_step * laa_fontsize)
+                    yy_shift = 0
+                    if plot_laa_names: 
+                        yy_shift = y_step * laa_fontsize
+                        plt.text(laa_centroids[count][0],laa_centroids[count][1]+y_shift+yy_shift,name,horizontalalignment=al_mode,fontsize=laa_fontsize*0.6,bbox=dict(boxstyle='square',color='#AAAA8877'))
+                    if plot_laa_values: plt.text(laa_centroids[count][0],laa_centroids[count][1]+y_shift-yy_shift,"%3.1f" % val,horizontalalignment=al_mode,fontsize=laa_fontsize,bbox=dict(boxstyle='square',color='#FFFFEE11'))
+        if add_date: plt.text(label_x,label_y,c_date.strftime("%B %d"), horizontalalignment=text_align_mode, style='italic',fontsize=date_font_size)
+        if add_title:plt.text(title_x,title_y,title_string,horizontalalignment=text_align_mode,fontsize=title_font_size)
+        if add_footer:    
+            footer = file_date.strftime("Based on LTLA and MSOA case data from coronavirus.data.gov.uk, data set published %d/%m/%y. github.com/jah-photoshop/autocovid")
+            fr_scale = f_scale
+            if(plot_laa_values): 
+                footer = "Values are cases/100K/week. "+footer
+                fr_scale = f_scale * 1.2
+            plt.text(frame_margins[1]-( (frame_margins[1] - frame_margins[0]) * 0.01),frame_margins[2]+( (frame_margins[3] - frame_margins[2]) * 0.01),footer,horizontalalignment='right',fontsize=title_font_size / fr_scale, bbox=dict(boxstyle='square',color='#AAAA8844'))
+        plt.savefig(f_string, bbox_inches='tight')
+        if post_process:
+            if resize_output: os.system('convert %s -resize %dx%d\! %s' % (f_string,target_width,target_height,f_string))
+            if add_background: os.system('composite %s %s %s' % (f_string,background_file,f_string))
+            if add_overlay: os.system('composite %s %s %s' % (overlay_filename, f_string,f_string))
+        fig.clf()    
+    print("________________________________________________________________________________")
+print("Operation complete.")
